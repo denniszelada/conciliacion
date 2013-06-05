3005dc4e0be8ad8e3d3a16018cdfbb05cdd6ea3d
Dennis Zelada <denniszelada@gmail.com>
2013-06-05 20:28:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conciliacion::Application.config.secret_token = 'd5ee2df9b30380ba293667c980e5e1b5babf6cf47b313fb90ba5926606255261a9b32716c6d002e734909761fe1992f0ce1c2d4aaa2c1a6524702898f831b9a0'
